fc40585e6bf1dce678158781e54e0b46ecab982e
sakhan-s <60611884+sakhan-s@users.noreply.github.com>
2020-10-09 21:09:07 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let firstStr = str;
+    const isMatches = (bracketPair) => {
+        return firstStr.includes(bracketPair.join(""));
+    };
+    while (bracketsConfig.some(isMatches)) {
+        bracketsConfig.forEach((configPair) => {
+            const secStr = configPair.join("");
+            while (firstStr.includes(secStr)) {
+                firstStr = firstStr.replace(secStr, "");
+            }
+        });
+    }
+    return firstStr.length === 0;
+};
